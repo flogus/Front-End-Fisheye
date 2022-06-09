edce4a7f72e2bbee1a5736cd5da60faa5ec6ac6a
flogus <fg@motionery.com>
2022-06-09 11:24:15 +0200

    feat: add console.info for the form data

--- scripts/utils/contactData.js
+++ scripts/utils/contactData.js
@@ -1,4 +1,17 @@
 const contactBtn = document.getElementById("contactButton");
-contactBtn.addEventListener("click", function () {
-  console.log("click buttin contact");
+contactBtn.addEventListener("click", function (event) {
+  let prenomValue = document.querySelector("#contact_modal #prenom").value;
+  let nomValue = document.querySelector("#contact_modal #nom").value;
+  let emailValue = document.querySelector("#contact_modal #email").value;
+  let msgValue = document.querySelector("#contact_modal #message").value;
+  console.clear();
+  console.group();
+  console.info("Fomulaire info");
+  console.info("prénom :", prenomValue);
+  console.info("nom :", nomValue);
+  console.info("e-mail :", emailValue);
+  console.info("message :", msgValue);
+  console.groupEnd();
+
+  event.preventDefault();
 });
